5fd155dd7350662b9eee65eb1a6edc7a875c0670
Sam Ess <samess.flowers@gmail.com>
2023-07-08 03:43:13 -0400

    adjust timing on plist, fix script

--- download_janitor.sh
+++ download_janitor.sh
@@ -3,30 +3,35 @@ set -euo pipefail
 
 sevendays=$( date -v-7d "+%m%d%H%M" )
 thirtydays=$( date -v-30d "+%m%d%H%M" )
-touch "/Users/$USER/Downloads/$sevendays"
-touch "/Users/$USER/Downloads/$thirtydays"
-touch -t "$sevendays" "/Users/$USER/Downloads/$sevendays"
-touch -t "$thirtydays" "/Users/$USER/Downloads/$thirtydays"
+touch "/tmp/$sevendays"
+touch "/tmp/$thirtydays"
+touch -t "$sevendays" "/tmp/$sevendays"
+touch -t "$thirtydays" "/tmp/$thirtydays"
 {
-if [[ ! -e "/Users/$USER/Downloads/old" ]]; then
-    mkdir "/Users/$USER/Downloads/old"
+if [[ ! -e "/Users/$USER/Downloads/.old" ]]; then
+    mkdir "/Users/$USER/Downloads/.old"
 fi
+echo "checking downloads"
 cd "/Users/$USER/Downloads/"
 for file in *; do
-    echo "$file"
-    if [[ "/Users/$USER/Downloads/$sevendays" -nt "$file" ]]; then
-        echo "$file is older"
-        #mv "$file" "/Users/$USER/Downloads/old/"
+    if [[ "/tmp/$sevendays" -nt "$file" ]]; then
+        echo "$file is 7+ days old"
+        mv "$file" "/Users/$USER/Downloads/.old/"
     fi
 done
-cd "/Users/$USER/Downloads/old"
-for file in *; do
-    if [[ "/Users/$USER/Downloads/$thirtydays" -nt "$file" ]]; then
-        #mv "/Users/$USER/Downloads/$file" "/Users/$USER/.Trash"
-    fi
-done
+cd "/Users/$USER/Downloads/.old"
+echo "checking old dir"
+if [[ $( ! find "/Users/$USER/Downloads/.old" -maxdepth 0 -empty -exec true \; ) ]]; then
+    for file in *; do
+        echo "$file"
+        if [[ "/tmp/$thirtydays" -nt "$file" ]]; then
+            echo "$file is older"
+            rm -r -- "/Users/$USER/Downloads/.old/$file"
+        fi
+    done
+fi
 } always {
-    $(rm "/Users/$USER/Downloads/$sevendays")
-    $(rm "/Users/$USER/Downloads/$thirtydays")
+    $( rm "/tmp/$sevendays" )
+    $( rm "/tmp/$thirtydays" )
 }
-
+say "run successful"
