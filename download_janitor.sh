c0908719900062d2d0e1a9de07d57c2b804dc2a2
Sam Ess <samess.flowers@gmail.com>
2023-07-11 19:52:14 -0400

    Whoops, it looks like everything in that last commit was broken

--- download_janitor.sh
+++ download_janitor.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
-set -euo pipefail
+set -euxo pipefail
 
 
-log = "/tmp/dj.log"
-write_log(text){
-    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - $text" >> $log
+log="/tmp/dj.log"
+function writelog() {
+    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - $1" >> $log
 }
 
-write_log("starting program")
+echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - starting program" >> $log
 sevendays=$( date -v-7d "+%m%d%H%M" )
 thirtydays=$( date -v-30d "+%m%d%H%M" )
 touch "/tmp/$sevendays"
@@ -16,27 +16,27 @@ touch -t "$sevendays" "/tmp/$sevendays"
 touch -t "$thirtydays" "/tmp/$thirtydays"
 
 if [[ ! -e "/Users/$USER/Downloads/.old" ]]; then
-    write_log("creating /Users/$USER/Downloads/.old")
+    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - creating /Users/$USER/Downloads/.old" >> $log
     mkdir "/Users/$USER/Downloads/.old"
 fi
-write_log("checking downloads")
+echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - checking downloads" >> $log
 cd "/Users/$USER/Downloads/"
 for file in *; do
     if [[ "/tmp/$sevendays" -nt "$file" ]]; then
-        write_log("$file is more than 7 days old")
+        echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - $file is more than seven days old" >> $log
         mv -- "$file" "/Users/$USER/Downloads/.old/"
     fi
 done
 cd "/Users/$USER/Downloads/.old"
-write_log("checking old dir")
+echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - checking .old dir" >> $log
 if [[ $( ! find "/Users/$USER/Downloads/.old" -maxdepth 0 -empty -exec true \; ) ]]; then
     for file in *; do
         if [[ "/tmp/$thirtydays" -nt "$file" ]]; then
-            write_log("$file is more than 30 days old")
+            echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - $file is more than 30 days old" >> $log
             rm -r -- "/Users/$USER/Downloads/.old/$file"
         fi
     done
 fi
 rm "/tmp/$sevendays"
 rm "/tmp/$thirtydays"
-write_log("run successful")
+echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - run successful" >> $log
