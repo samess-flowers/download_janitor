37f4b2a62b88037ca79fe82e20eff5454a85ab7d
Sam Ess <samess.flowers@gmail.com>
2023-07-11 19:36:48 -0400

    Add logging to see if damn thing works

--- download_janitor.sh
+++ download_janitor.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 set -euo pipefail
 
+
+log = "/tmp/dj.log"
+write_log(text){
+    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") - $text" >> $log
+}
+
+write_log("starting program")
 sevendays=$( date -v-7d "+%m%d%H%M" )
 thirtydays=$( date -v-30d "+%m%d%H%M" )
 touch "/tmp/$sevendays"
@@ -9,26 +16,27 @@ touch -t "$sevendays" "/tmp/$sevendays"
 touch -t "$thirtydays" "/tmp/$thirtydays"
 
 if [[ ! -e "/Users/$USER/Downloads/.old" ]]; then
+    write_log("creating /Users/$USER/Downloads/.old")
     mkdir "/Users/$USER/Downloads/.old"
 fi
-echo "checking downloads"
+write_log("checking downloads")
 cd "/Users/$USER/Downloads/"
 for file in *; do
     if [[ "/tmp/$sevendays" -nt "$file" ]]; then
-        echo "$file is 7+ days old"
-        mv "$file" "/Users/$USER/Downloads/.old/"
+        write_log("$file is more than 7 days old")
+        mv -- "$file" "/Users/$USER/Downloads/.old/"
     fi
 done
 cd "/Users/$USER/Downloads/.old"
-echo "checking old dir"
+write_log("checking old dir")
 if [[ $( ! find "/Users/$USER/Downloads/.old" -maxdepth 0 -empty -exec true \; ) ]]; then
     for file in *; do
-        echo "$file"
         if [[ "/tmp/$thirtydays" -nt "$file" ]]; then
-            echo "$file is older"
+            write_log("$file is more than 30 days old")
             rm -r -- "/Users/$USER/Downloads/.old/$file"
         fi
     done
 fi
 rm "/tmp/$sevendays"
 rm "/tmp/$thirtydays"
+write_log("run successful")
